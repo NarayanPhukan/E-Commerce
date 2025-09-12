311f1059b97eefef8dfc5bd27f611c056feb18d4
NarayanPhukan <narayanphukan30@gmail.com>
2025-09-12 23:36:55 +0530

    No nav

--- E-Commerce/frontend/src/App.jsx
+++ E-Commerce/frontend/src/App.jsx
@@ -12,13 +12,13 @@ import PlaceOrder from './Pages/PlaceOrder';
 import Navbar from './Components/Navbar';
 import Footer from './Components/Footer';
 import SearchBar from './Components/SearchBar';
-import { Analytics } from "@vercel/analytics/react"
+// import { Analytics } from "@vercel/analytics/react/"
 
 const App = () => {
   return (
     
       <div className='px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]'>
-        <Navbar />
+        {/* <Navbar /> */}
         <SearchBar />
         <Routes>
         
