234da7b2f831206c4b6af91227260b74252765c7
NarayanPhukan <narayanphukan30@gmail.com>
2025-09-18 08:51:58 +0530

    ToArch

--- E-Commerce/frontend/src/Pages/Cart.jsx
+++ E-Commerce/frontend/src/Pages/Cart.jsx
@@ -51,7 +51,7 @@ const Cart = () => {
                     </div>
                   </div>
                 </div>
-                <input onChange={(e) => e.target.value === '' || e.target.value === 0} type="number" min={1} defaultValue={item.quantity} className='border max-w-10 sm:max-w-20 px-1 sm:px-2 py-1' />
+                <input onChange={(e) => e.target.value === '' || e.target.value === '0' ? null : updateQuantity(item._id, item.size, Number(e.target.value)) } type="number" min={1} defaultValue={item.quantity} className='border max-w-10 sm:max-w-20 px-1 sm:px-2 py-1' />
                 <img src={assets.bin_icon} className='w-4 mr-4 sm:w-5 cursor-pointer' alt="" onClick={() => updateQuantity(item._id, item.size, 0)} />
               </div>
             )
