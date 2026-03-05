73daf76e05af34093a93e7f7ab44f2b4a0e5bc10
NarayanPhukan <narayanphukan30@gmail.com>
2026-03-05 19:23:16 +0530

    Initial commit: E-commerce Frontend application

---
+++ ecommerce/frontend/src/components/SearchBar.jsx
@@ -0,0 +1,22 @@
+import React, { useContext } from 'react';
+import { ShopContext } from '../context/ShopContext';
+import { assets } from '../assets/assets';
+import { useNavigate } from 'react-router-dom';
+
+ const SearchBar = () => {
+
+    const { search, setSearch, showSearch, setShowSearch } = useContext(ShopContext);
+    const navigate = useNavigate();
+
+    return showSearch ? (
+        <div className='border-t border-b border-gray-50 text-center'>
+            <div className='inline-flex items-center justify-center border border-gray-400 px-5 py-2 my-5 mx-3 rounded-full w-3/4 sm:w-1/2'>
+                <input type="text" placeholder='Search for products...' className='flex-1 outline-none bg-inherit text-sm' value={search} onChange={(e) => setSearch(e.target.value)} onKeyDown={(e) => { if (e.key === 'Enter') { navigate('/collection'); setShowSearch(false); } }}/>
+                <img src={assets.search_icon} className='w-4' alt="Search" />
+            </div>
+            <img src={assets.cross_icon} className='w-3 inline cursor-pointer' onClick={() => setShowSearch(false)} alt="Close" />
+        </div>
+    ) : null;
+};
+
+export default SearchBar
