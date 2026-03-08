5ae095b5ba74debbb6f5afc0a7d6d43b923e6b77
NarayanPhukan <narayanphukan30@gmail.com>
2026-03-08 19:23:42 +0530

    addToCart not complete'

--- ecommerce/frontend/src/components/SearchBar.jsx
+++ ecommerce/frontend/src/components/SearchBar.jsx
@@ -1,14 +1,21 @@
-import React, { useContext } from 'react';
+import React, { useEffect, useContext } from 'react';
 import { ShopContext } from '../context/ShopContext';
 import { assets } from '../assets/assets';
-import { useNavigate } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
 
  const SearchBar = () => {
 
     const { search, setSearch, showSearch, setShowSearch } = useContext(ShopContext);
+    const location = useLocation()
     const navigate = useNavigate();
 
-    return showSearch ? (
+    useEffect(() => {
+        if (location.pathname !== '/collection') {
+            setShowSearch(false);
+        }
+    }, [location, setShowSearch]);
+
+    return (location.pathname === '/collection' && showSearch) ? (
         <div className='border-t border-b border-gray-50 text-center'>
             <div className='inline-flex items-center justify-center border border-gray-400 px-5 py-2 my-5 mx-3 rounded-full w-3/4 sm:w-1/2'>
                 <input type="text" placeholder='Search for products...' className='flex-1 outline-none bg-inherit text-sm' value={search} onChange={(e) => setSearch(e.target.value)} onKeyDown={(e) => { if (e.key === 'Enter') { navigate('/collection'); setShowSearch(false); } }}/>
